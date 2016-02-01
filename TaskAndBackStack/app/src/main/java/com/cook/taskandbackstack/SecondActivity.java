52cbcfdac018a94057fd959bc53772601c6a8262
Roman Kukhar <kukharroma@gmail.com>
2016-02-01 11:04:38 +0200

    show three task in overview screen

--- TaskAndBackStack/app/src/main/java/com/cook/taskandbackstack/SecondActivity.java
+++ TaskAndBackStack/app/src/main/java/com/cook/taskandbackstack/SecondActivity.java
@@ -1,16 +1,18 @@
 package com.cook.taskandbackstack;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by roma on 01.02.16.
+ */
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, SecondActivity.class);
+        Intent intent = new Intent(this, ThirdActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
         startActivity(intent);
     }
---
+++ TaskAndBackStack/app/src/main/java/com/cook/taskandbackstack/ThirdActivity.java
@@ -0,0 +1,10 @@
+package com.cook.taskandbackstack;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by roma on 01.02.16.
+ */
+public class ThirdActivity extends AppCompatActivity{
+
+}
