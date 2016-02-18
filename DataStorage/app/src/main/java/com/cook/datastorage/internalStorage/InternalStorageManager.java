666b44ce7f0861a3490012589882595166c580dc
Roman Kukhar <kukharroma@gmail.com>
2016-02-18 17:59:26 +0200

    [DataStorage]

--- DataStorage/app/src/main/java/com/cook/datastorage/internalStorage/InternalStorageManager.java
+++ DataStorage/app/src/main/java/com/cook/datastorage/internalStorage/InternalStorageManager.java
@@ -3,6 +3,7 @@ package com.cook.datastorage.internalStorage;
 import android.content.Context;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,6 +11,12 @@ import java.io.InputStreamReader;
 
 /**
  * Created by roma on 18.02.16.
+ *
+ * You can save files directly on the device's internal storage.
+ * By default, files saved to the internal storage are private to your
+ * application and other applications cannot access them (nor can the user).
+ * When the user uninstalls your application, these files are removed.
+ *
  */
 public class InternalStorageManager {
 
@@ -50,5 +57,36 @@ public class InternalStorageManager {
 
         }
         return name.toString();
+    }
+
+    /**
+     *
+     * @return Gets the absolute path to the filesystem directory where your internal files are saved.
+     */
+    public static File getDirCache(){
+        return context.getFilesDir();
+    }
+
+    /**
+     *
+     * @return Creates (or opens an existing) directory within your internal storage space.
+     */
+    public static File getDir(String fileName){
+        return context.getDir(fileName, Context.MODE_PRIVATE);
+    }
+
+    /**
+     * Deletes a file saved on the internal storage.
+     * @param fileName name of dir
+     */
+    public static void deleteFile(String fileName){
+        context.deleteFile(fileName);
+    }
+
+    /**
+     * @return  Returns an array of files currently saved by your application.
+     */
+    public static String[] fileList(){
+       return context.fileList();
     }
 }
