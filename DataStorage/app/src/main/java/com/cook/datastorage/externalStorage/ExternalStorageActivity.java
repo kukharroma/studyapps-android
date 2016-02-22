a8e719d63b2a86fec7ccfb16611efaee1a4f704f
Roman Kukhar <kukharroma@gmail.com>
2016-02-22 18:03:20 +0200

    [DataStorage]

--- DataStorage/app/src/main/java/com/cook/datastorage/externalStorage/ExternalStorageActivity.java
+++ DataStorage/app/src/main/java/com/cook/datastorage/externalStorage/ExternalStorageActivity.java
@@ -10,7 +10,6 @@ import android.widget.Toast;
 
 import com.cook.datastorage.R;
 import com.cook.datastorage.databinding.ActivityExternalStorageBinding;
-import com.cook.datastorage.internalStorage.InternalStorageManager;
 
 /**
  * Created by roma on 19.02.16.
@@ -26,32 +25,36 @@ public class ExternalStorageActivity extends AppCompatActivity {
 
     }
 
-    public class ExternalStorageViewModel{
+    public class ExternalStorageViewModel {
 
         public ObservableField<String> text = new ObservableField<>();
 
         public ExternalStorageViewModel() {
         }
 
-        public void save(View view){
+        public void saveInExternalFile(View view) {
             Toast.makeText(getApplicationContext(), "Text has been saved", Toast.LENGTH_LONG).show();
-            ExternalStorageManager.();
+            ExternalStorageManager.saveInExternalDir(binding.etFileData.getText().toString());
             text.set("");
         }
 
-        public void load(View view){
+        public void loadInExternalDir(View view) {
 
         }
 
-        public void getDir(View view){
+        public void load(View view) {
 
         }
 
-        public void getCahceDir(View view){
+        public void getDir(View view) {
 
         }
 
-        public void fileList(View view){
+        public void getCahceDir(View view) {
+
+        }
+
+        public void fileList(View view) {
 
         }
 
--- DataStorage/app/src/main/java/com/cook/datastorage/externalStorage/ExternalStorageManager.java
+++ DataStorage/app/src/main/java/com/cook/datastorage/externalStorage/ExternalStorageManager.java
@@ -2,9 +2,15 @@ package com.cook.datastorage.externalStorage;
 
 import android.content.Context;
 import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by roma on 19.02.16.
@@ -15,8 +21,8 @@ public class ExternalStorageManager {
     private static final String EXTERNAL_FILE_NAME = "EXTERNAL_FILE_NAME";
     private static Context context;
 
-    public static void init(Context context){
-        ExternalStorageManager.context  = context;
+    public static void init(Context context) {
+        ExternalStorageManager.context = context;
     }
 
     /**
@@ -33,7 +39,7 @@ public class ExternalStorageManager {
     /**
      * Checks if external storage is available to at least read
      */
-    public  static boolean isExternalStorageReadable() {
+    public static boolean isExternalStorageReadable() {
         String state = Environment.getExternalStorageState();
         if (Environment.MEDIA_MOUNTED.equals(state) ||
                 Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
@@ -59,12 +65,57 @@ public class ExternalStorageManager {
         return file;
     }
 
-    public static File getExternalFilesDir(){
+    public static File getExternalDir() {
         return context.getExternalFilesDir(EXTERNAL_FILE_NAME);
     }
 
-    public static File getExternalCacheDir(){
+    public static File getExternalCacheDir() {
         return context.getExternalCacheDir();
     }
+
+    public static void saveInExternalDir(String text) {
+        save(text, getExternalDir());
+    }
+
+    public static String loadExternalDir() {
+        return load(getExternalDir());
+    }
+
+    public static void saveInExternalCacheDir(String text) {
+        save(text, getExternalCacheDir());
+    }
+
+    public static String loadInternalCacheDir() {
+        return load(getExternalCacheDir());
+    }
+
+    private static void save(String text, File file) {
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(text.getBytes());
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String load(File file) {
+        StringBuilder name = new StringBuilder();
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                name.append(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+
+        }
+        return name.toString();
+    }
+
+
 
 }
