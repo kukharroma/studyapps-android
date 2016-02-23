a9eb7c7b02931fbb64506c968363d297a6776a3a
Roman Kukhar <kukharroma@gmail.com>
2016-02-23 13:50:22 +0200

    [DataStorage]

--- DataStorage/app/src/main/java/com/cook/datastorage/externalStorage/ExternalStorageActivity.java
+++ DataStorage/app/src/main/java/com/cook/datastorage/externalStorage/ExternalStorageActivity.java
@@ -10,6 +10,10 @@ import android.widget.Toast;
 
 import com.cook.datastorage.R;
 import com.cook.datastorage.databinding.ActivityExternalStorageBinding;
+import com.cook.datastorage.helper.FileHelper;
+import com.cook.datastorage.internalStorage.InternalStorageManager;
+
+import org.apache.commons.io.FileUtils;
 
 /**
  * Created by roma on 19.02.16.
@@ -22,40 +26,46 @@ public class ExternalStorageActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_external_storage);
-
+        binding.setViewModel(new ExternalStorageViewModel());
     }
 
     public class ExternalStorageViewModel {
 
-        public ObservableField<String> text = new ObservableField<>();
+        public ObservableField<String> data = new ObservableField<>();
+        public ObservableField<String> tvExternalDir = new ObservableField<>();
+        public ObservableField<String> tvExternalCacheDir = new ObservableField<>();
+        public ObservableField<String> tvLoadExternalDir = new ObservableField<>();
+        public ObservableField<String> tvLoadExternalCacheDir = new ObservableField<>();
 
         public ExternalStorageViewModel() {
         }
 
-        public void saveInExternalFile(View view) {
-            Toast.makeText(getApplicationContext(), "Text has been saved", Toast.LENGTH_LONG).show();
-            ExternalStorageManager.saveInExternalDir(binding.etFileData.getText().toString());
-            text.set("");
-        }
-
-        public void loadInExternalDir(View view) {
-
-        }
-
-        public void load(View view) {
-
-        }
-
         public void getDir(View view) {
-
+            tvExternalDir.set(ExternalStorageManager.getExternalDir().getAbsolutePath());
         }
 
         public void getCahceDir(View view) {
-
+            tvExternalDir.set(ExternalStorageManager.getExternalCacheDir().getAbsolutePath());
         }
 
-        public void fileList(View view) {
+        public void saveInExternalDir(View view) {
+            Toast.makeText(getApplicationContext(), "Text has been saved", Toast.LENGTH_LONG).show();
+            ExternalStorageManager.saveInExternalDir(binding.etFileData.getText().toString());
+            data.set("");
+        }
 
+        public void loadExternalDir(View view) {
+            tvLoadExternalDir.set(FileHelper.loadFromFile(ExternalStorageManager.getExternalDir()));
+        }
+
+        public void saveInExternalCacheDir(View view) {
+            Toast.makeText(getApplicationContext(), "Text has been saved in external cache dir", Toast.LENGTH_LONG).show();
+            ExternalStorageManager.saveInExternalCacheDir(binding.etFileData.getText().toString());
+            data.set("");
+        }
+
+        public void loadExternalCahceDir(View view) {
+            tvLoadExternalDir.set(FileHelper.loadFromFile(ExternalStorageManager.getExternalCacheDir()));
         }
 
     }
--- DataStorage/app/src/main/java/com/cook/datastorage/helper/FileHelper.java
+++ DataStorage/app/src/main/java/com/cook/datastorage/helper/FileHelper.java
@@ -40,4 +40,3 @@ public class FileHelper {
         return name.toString();
     }
 }
-}
