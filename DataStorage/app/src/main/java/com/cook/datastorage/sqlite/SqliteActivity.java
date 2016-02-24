a60ba101e96e53e2b29685b4d5cd975ceba5b070
Roman Kukhar <kukharroma@gmail.com>
2016-02-24 14:14:09 +0200

    [DataStorage]

--- DataStorage/app/src/main/java/com/cook/datastorage/sqlite/SqliteActivity.java
+++ DataStorage/app/src/main/java/com/cook/datastorage/sqlite/SqliteActivity.java
@@ -1,7 +1,16 @@
 package com.cook.datastorage.sqlite;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by roma on 24.02.16.
  */
-public class SqliteActivity {
+public class SqliteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
